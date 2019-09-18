b09fdcf99400f7977f20e8dfd8514559b88bb348
Krzysztof Handzlik <krzysztofhandzlik89@gmail.com>
2019-09-18 08:19:14 +0200

    CPU bound project add

---
+++ CPU_bound_async_await/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPU_bound_async_await
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var totalAfterTax = CalculateSomethingAsync(70);
+            DoSomethingSynchronous();
+
+            totalAfterTax.Wait();
+            Console.ReadLine();
+        }
+
+        private static void DoSomethingSynchronous()
+        {
+            Console.WriteLine("Doing some synchronous work");
+        }
+
+        static async Task<float> CalculateSomethingAsync(float value)
+        {
+            Console.WriteLine("CPU asynchronous task");
+            var result = await Task.Run(() => value * 1.2f);
+            Console.WriteLine($"Finished Task. Total of ${value} after calculation is ${result} ");
+            return result;
+        }
+    }
+}

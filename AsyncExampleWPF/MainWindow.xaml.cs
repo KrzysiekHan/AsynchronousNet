53fd9be3034ca565c1f065042ea3b9a8b394a763
Krzysztof Handzlik <krzysztofhandzlik89@gmail.com>
2019-09-22 20:54:27 +0200

    synchronous part

--- AsyncExampleWPF/MainWindow.xaml.cs
+++ AsyncExampleWPF/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net.Http;
+using System.Net;
+using System.IO;
 
 namespace AsyncExampleWPF
 {
@@ -24,5 +27,65 @@ namespace AsyncExampleWPF
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            resultsTextBox.Clear();
+            SumPageSizes();
+            resultsTextBox.Text += "\r\nControl returned to startButton_Click.";
+        }
+
+        private void SumPageSizes()
+        {
+            List<string> urlList = SetUpURLList();
+            var total = 0;
+            foreach (var url in urlList)
+            {
+                byte[] urlContents = GetURLContents(url);
+                DisplayResults(url, urlContents);
+                total += urlContents.Length;
+            }
+            resultsTextBox.Text += $"\r\n\r\nTotal bytes returned: {total} \r\n";
+        }
+
+        private List<string> SetUpURLList()
+        {
+            var urls = new List<string>
+            {
+                "https://msdn.microsoft.com/library/windows/apps/br211380.aspx",
+                "https://msdn.microsoft.com",
+                "https://msdn.microsoft.com/library/hh290136.aspx",
+                "https://msdn.microsoft.com/library/ee256749.aspx",
+                "https://msdn.microsoft.com/library/hh290138.aspx",
+                "https://msdn.microsoft.com/library/hh290140.aspx",
+                "https://msdn.microsoft.com/library/dd470362.aspx",
+                "https://msdn.microsoft.com/library/aa578028.aspx",
+                "https://msdn.microsoft.com/library/ms404677.aspx",
+                "https://msdn.microsoft.com/library/ff730837.aspx"
+            };
+            return urls;
+        }
+
+        private byte[] GetURLContents(string url)
+        {
+            var content = new MemoryStream();
+            var webReq = (HttpWebRequest)WebRequest.Create(url);
+            using (WebResponse response = webReq.GetResponse())
+            {
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    responseStream.CopyTo(content);
+                }
+                return content.ToArray();
+            }
+        }
+
+        private void DisplayResults(string url, byte[] content)
+        {
+            var bytes = content.Length;
+            var displayURL = url.Replace("https://", "");
+            resultsTextBox.Text += $"\n{displayURL,-58} {bytes,8}";
+        }
+
     }
 }
